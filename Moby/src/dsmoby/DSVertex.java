7d0a2e1cf01dc2d19b96234fece5eb1bc7bde035
Rob Hochberg <csudallas@gmail.com>
2013-04-24 18:54:30 -0500

    Some changes to the DSGraph and DSVertex classes Added a GraphTester class

--- Moby/src/dsmoby/DSVertex.java
+++ Moby/src/dsmoby/DSVertex.java
@@ -4,8 +4,24 @@ public class DSVertex implements Comparable<DSVertex>{
 	String label;
 	DSLinkedList<DSVertex> neighbors;
 
+	/*
+	 * Constructs an unlabeled vertex
+	 */
+	public DSVertex(){
+		this("");
+	}
 	
+	/*
+	 * Constructs a vertex with label l and empty neighbors list.
+	 */
+	public DSVertex(String l){
+		label = l;
+		neighbors = new DSLinkedList<DSVertex>();
+	}
 	
+	/*
+	 * 
+	 */
 	public int compareTo(DSVertex v){
 		return label.compareTo(v.label);
 	}
---
+++ Moby/src/dsmoby/GraphTester.java
@@ -0,0 +1,10 @@
+package dsmoby;
+
+public class GraphTester {
+
+	public static void main(String[] args) {
+		DSGraph g= new DSGraph("house.graph");
+		g.printGraph();
+	}
+
+}
